c4e1e6184361c8d1c461f529e3e14ae6d5b62705
mykim <mykim8710@gmail.com>
2022-08-18 23:16:36 +0900

    [ dev, study ] Servlet - 서블릿 예제 - HttpServletRequest ㄴ HTTP 요청 데이터 - GET 쿼리 파라미터 ㄴ HTTP 요청 데이터 - POST HTML form data ㄴ HTTP 요청 데이터 - API 메시지 바디 - 단순 텍스트 ㄴ HTTP 요청 데이터 - API 메시지 바디 - JSON - HttpServletResponse ㄴ HTTP 응답 데이터 - 단순 텍스트 ㄴ HTTP 응답 데이터 - HTML ㄴ HTTP 응답 데이터 - JSON

--- src/main/java/com/example/servlet/ServletStudyApplication.java
+++ src/main/java/com/example/servlet/ServletStudyApplication.java
@@ -2,12 +2,12 @@ package com.example.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan   // 서블릿 자동등록
 @SpringBootApplication
 public class ServletStudyApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ServletStudyApplication.class, args);
     }
-
 }
